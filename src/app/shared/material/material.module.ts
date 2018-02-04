eeeefeb87c109136ce6b36eff5252a962f8540c9
Zabamund <fracgeol@gmail.com>
2018-02-04 11:00:59 +0100

    adding Entypo, setting up social links in navbar

--- src/app/shared/material/material.module.ts
+++ src/app/shared/material/material.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+
+
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    MatToolbarModule,
+    MatIconModule
   ],
-  declarations: []
+  declarations: [],
+  exports: [
+    MatToolbarModule,
+    MatIconModule
+  ]
 })
 export class MaterialModule { }
