8725d960d51850ac078566a1a836ce0075e1d444
Zabamund <fracgeol@gmail.com>
2018-02-05 19:46:28 +0100

    implementing translation service in header

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { TranslationService } from '../shared';
 
 @Component({
   selector: 'ocio-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent {
 
-  constructor() { }
+  constructor(
+    private translationService: TranslationService
+  ) { }
 
-  ngOnInit() {
+  switchLanguage(language: string) {
+    this.translationService.switchLanguage(language);
   }
 
 }
