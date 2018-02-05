cec790f06e52f834e22cc96389af6a3b5878dae5
Zabamund <fracgeol@gmail.com>
2018-02-05 19:45:23 +0100

    importing translate and httpclient modules and exporting function for AoT

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,20 @@ import { MaterialModule } from './shared/material';
 
 import { AppRoutingModule } from './app-routing.module';
 
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header';
 import { ContentComponent } from './content';
+
+import { TranslationService } from './shared';
+
+// AoT requires an exported function for factories
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http);
+}
 
 @NgModule({
   declarations: [
@@ -20,9 +31,19 @@ import { ContentComponent } from './content';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MaterialModule
+    MaterialModule,
+    TranslateModule.forRoot({
+      loader: {
+          provide: TranslateLoader,
+          useFactory: HttpLoaderFactory,
+          deps: [HttpClient]
+      }
+    }),
+    HttpClientModule,
   ],
-  providers: [],
+  providers: [
+    TranslationService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
