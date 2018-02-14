dc5d5500b2563b54a9f2cf1f38664549fd4e5ff5
Zabamund <fracgeol@gmail.com>
2018-02-14 10:06:09 +0100

    adding function in *.ts to add trustmark, needed to correct script

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -12,4 +12,13 @@ export class FooterComponent implements OnInit {
   ngOnInit() {
   }
 
+  async addTrustMark() {
+    try {
+      const v = document.createElement('script');
+      v.setAttribute('type', 'text/javascript');
+      v.setAttribute('src', '//cdn.ywxi.net/js/host-loader.js?h=' + 'https://obsidiancode.io');
+      document.getElementsByTagName('head')[0].appendChild(v);
+    } catch (e) {}
+  }
+
 }
