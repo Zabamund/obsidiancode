0d1fe93dd72d547aa7e732a978496c5db1cbdea5
Zabamund <fracgeol@gmail.com>
2018-02-20 22:48:11 +0100

    update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,9 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+
+  dialogflow: {
+    ObsidianBot: '42e6486d429a4a6fa73a98c5352c14cc'
+  }
 };
