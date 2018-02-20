1b8167f8d2e23a9a076c30a530620a5b3192663b
Zabamund <fracgeol@gmail.com>
2018-02-20 23:51:15 +0100

    set up the chat service

--- src/app/chat/chat.service.ts
+++ src/app/chat/chat.service.ts
@@ -1,8 +1,45 @@
 import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+
+import { ApiAiClient } from 'api-ai-javascript';
+
+import { Observable } from 'rxjs/Observable';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+import { constants } from 'os';
+
+export class Message {
+  constructor(
+    public content: string,
+    public sentBy: string
+  ) {}
+}
 
 @Injectable()
 export class ChatService {
 
+  readonly token = environment.dialogflow.ObsidianBot;
+  readonly client = new ApiAiClient({ accessToken: this.token });
+
+  conversation = new BehaviorSubject<Message[]>([]);
+
   constructor() { }
+
+  // Adds message to source
+  update(msg: Message) {
+    this.conversation.next([msg]);
+  }
+
+  // Sends and receives messages via DialogFlow
+  converse(msg: string) {
+    const userMessage = new Message(msg, 'user');
+    this.update(userMessage);
+
+    return this.client.textRequest(msg)
+                .then(res => {
+                  const speech = res.result.fulfillment.speech;
+                  const botMessage = new Message(speech, 'bot');
+                });
+  }
+
 
 }
